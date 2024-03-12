ff4969b3e9efb6e4c2c544b782c357b92b312b92
jkh675 <rluo2018@gmail.com>
2024-03-12 16:48:25 +0800

    MUI setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter as inter } from "next/font/google";
 import "./globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 
 const INTER = inter({ subsets: ["latin"] });
 
