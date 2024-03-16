2e5772bd69a47761ab26b200e561bb60b4974863
jkh675 <rluo2018@gmail.com>
2024-03-16 11:21:08 +0800

    fix the wrong file name

--- src/components/GlobalLayout/GlobalLayoutSideBar.tsx
+++ src/components/GlobalLayout/GlobalLayoutSideBar.tsx
@@ -1,16 +1,14 @@
 import { Inbox } from "@mui/icons-material";
 import {
-    Container,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    SwipeableDrawer,
+	List,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	SwipeableDrawer,
 } from "@mui/material";
 import { usePathname } from "next/navigation";
 import React from "react";
-import { NextLinkComposed } from "../NextJSLint";
+import { NextLinkComposed } from "../NextJSLink";
 
 export interface GlobalLayoutSideBarProps {
     open: boolean;
@@ -19,61 +17,61 @@ export interface GlobalLayoutSideBarProps {
 }
 
 const ROUTE_LIST = [
-    {
-        title: "Home page",
-        icon: <Inbox />,
-        url: "/",
-    },
-    {
-        title: "Home page",
-        icon: <Inbox />,
-        url: "/",
-    },
+	{
+		title: "Home page",
+		icon: <Inbox />,
+		url: "/",
+	},
+	{
+		title: "Home page",
+		icon: <Inbox />,
+		url: "/",
+	},
 ];
 
 export default function GlobalLayoutSideBar(props: GlobalLayoutSideBarProps) {
-    const [selectedIndex, setSelectedIndex] = React.useState(0);
-    const pathname = usePathname();
+	const [selectedIndex, setSelectedIndex] = React.useState(0);
+	const pathname = usePathname();
 
-    React.useEffect(() => {
-        setSelectedIndex(ROUTE_LIST.findIndex((v) => (v.url == pathname)));
-    },[])
+	React.useEffect(() => {
+		setSelectedIndex(ROUTE_LIST.findIndex((v) => (v.url == pathname)));
+	},[]);
 
-    return (
-        <>
-            <SwipeableDrawer
-                anchor="left"
-                open={props.open}
-                onClose={props.onClose}
-                onOpen={props.onOpen}
-                ModalProps={{
-                    keepMounted: true,
-                }}
-                sx={{ m: 0, p: 0 }}
-            >
-                {/* <Container maxWidth="100px" sx={{ m: 0, p: 0 }}> */}
-                    <List sx={{ m: 0, p: 0, width: 200}}>
-                        {ROUTE_LIST.map((v, k) => {
-                            return (
-                                <ListItemButton
-                                    key={k}
-                                    selected={selectedIndex === k}
-                                    onClick={(event) => {
-                                        setSelectedIndex(k);
-                                    }}
-                                    component={NextLinkComposed}
-                                    to={{
-                                        pathname: v.url,
-                                    }}
-                                >
-                                    <ListItemIcon>{v.icon}</ListItemIcon>
-                                    <ListItemText primary={v.title} />
-                                </ListItemButton>
-                            );
-                        })}
-                    </List>
-                {/* </Container> */}
-            </SwipeableDrawer>
-        </>
-    );
+	return (
+		<>
+			<SwipeableDrawer
+				anchor="left"
+				open={props.open}
+				onClose={props.onClose}
+				onOpen={props.onOpen}
+				ModalProps={{
+					keepMounted: true,
+				}}
+				sx={{ m: 0, p: 0 }}
+			>
+				{/* <Container maxWidth="100px" sx={{ m: 0, p: 0 }}> */}
+				<List sx={{ m: 0, p: 0, width: 200}}>
+					{ROUTE_LIST.map((v, k) => {
+						return (
+							<ListItemButton
+								key={k}
+								selected={selectedIndex === k}
+								onClick={() => {
+									setSelectedIndex(k);
+								}}
+								component={NextLinkComposed}
+								to={{
+									pathname: v.url,
+								}}
+							>
+								<ListItemIcon>{v.icon}</ListItemIcon>
+								<ListItemText primary={v.title} />
+							</ListItemButton>
+						);
+					})}
+				</List>
+				{/* </Container> */}
+			</SwipeableDrawer>
+		</>
+	);
 }
