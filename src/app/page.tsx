739ddd037a99525e06e2872d36580f863ff3212b
jkh675 <rluo2018@gmail.com>
2024-03-17 10:49:39 +0800

    fix the styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import styles from "./page.module.css";
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
+		<>
 			<h1>Wellcome to IPSC smart system</h1>
 			<h1>Click the top-left button to navigate to different pages!</h1>
-		</main>
+		</>
 	);
 }
