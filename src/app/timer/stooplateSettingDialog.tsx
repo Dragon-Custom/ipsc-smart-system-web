c98b1f87ebb6f28f2acf32b0279e6de87e6df397
jkh675 <rluo2018@gmail.com>
2024-03-27 11:49:52 +0800

    impl the UI

--- src/app/timer/stooplateSettingDialog.tsx
+++ src/app/timer/stooplateSettingDialog.tsx
@@ -1,3 +1,4 @@
+import { BUZZER_WAVEFORM_OBJECT } from "@/buzzer";
 import {
 	Button,
 	Dialog,
@@ -5,9 +6,13 @@ import {
 	DialogContent,
 	DialogProps,
 	DialogTitle,
+	FormControl,
 	Grid,
 	Input,
+	InputLabel,
+	MenuItem,
 	Paper,
+	Select,
 	Slider,
 	Stack,
 	Typography,
@@ -17,7 +22,7 @@ import React from "react";
 interface SettingItemProps extends SettingItem {
     //when slider or numeric input the value will be the value of component
     //when selector the value will be the index of the selected item
-    onChange: (setting_name: string, value: number) => unknown;
+    onChange: (setting_name: string, value: number | number[]) => unknown;
 }
 function SettingItem(props: SettingItemProps) {
 
@@ -25,15 +30,35 @@ function SettingItem(props: SettingItemProps) {
 		props.onChange(props.setting_name, parseFloat(v as string));
 	}
 
+	function onRangeChange(v: number[]) {
+		props.onChange(props.setting_name, v);
+	}
+
+
 	return (
 		<>
-			<Paper elevation={5} sx={{ p: 2 }}>
+			<Paper elevation={5} sx={{ px: 2, pt: 1, my:1}}>
 				<Stack>
 					<Typography align="center">{props.display_name}</Typography>
 					{(() => {
 						switch (props.type) {
 						case "selector":
-							return <>dwa</>;
+							return (
+								<>
+									<FormControl fullWidth>
+										<InputLabel>Waveform</InputLabel>
+										<Select
+											value={props.value as number}
+											label="Waveform"
+											onChange={(v) => onChange(v.target.value)}
+										>
+											{props.selector_item?.map((v, k) => (
+												<MenuItem value={k} key={k}>{v}</MenuItem>
+											))}
+										</Select>
+									</FormControl>
+								</>
+							);
 						case "slider":
 							return (
 								<>
@@ -48,6 +73,7 @@ function SettingItem(props: SettingItemProps) {
 												}
 												min={props.min}
 												max={props.max}
+												step={0.1}
 											/>
 										</Grid>
 										<Grid item xs={2}>
@@ -65,6 +91,66 @@ function SettingItem(props: SettingItemProps) {
 													min: props.min,
 													max: props.max,
 												}}
+												endAdornment={props.unit}
+												inputMode="decimal"
+											/>
+										</Grid>
+									</Grid>
+								</>
+							);
+						case "range":
+							return (
+								<>
+									<Grid container gap={1}>
+										<Grid item xs={2}>
+											<Input
+												sx={{mr: 1}}
+												fullWidth
+												type="number"
+												value={(props.value as number[])[0]}
+												onChange={(v) =>
+													onChange(
+														v.currentTarget.value,
+													)
+												}
+												inputProps={{
+													min: props.min,
+													max: props.max,
+												}}
+												inputMode="decimal"
+												endAdornment={props.unit}
+											/>
+										</Grid>
+										<Grid item xs={7}>
+											<Slider
+												value={props.value}
+												onChange={(e, v) =>
+													onRangeChange(
+														v as number[],
+													)
+												}
+												min={props.min}
+												max={props.max}
+												valueLabelDisplay="auto"
+												step={0.1}
+											/>
+										</Grid>
+										<Grid item xs={2}>
+											<Input
+												sx={{ml: 1}}
+												fullWidth
+												type="number"
+												value={(props.value as number[])[1]}
+												onChange={(v) =>
+													onChange(
+														v.currentTarget.value,
+													)
+												}
+												inputProps={{
+													min: props.min,
+													max: props.max,
+												}}
+												endAdornment={props.unit}
 												inputMode="decimal"
 											/>
 										</Grid>
@@ -83,26 +169,62 @@ function SettingItem(props: SettingItemProps) {
 interface SettingItem {
 	setting_name: string;
     display_name: string;
-    type: "slider" | "selector"
+    type: "slider" | "selector" | "range"
     max?: number;
     min?: number;
 	selector_item?: string[];
-    value: number;
+	value: number | number[];
+	unit?: string;
 }
 export default function StopplateSettngDialog(props: DialogProps) {
 
 	const [setting, setSetting] = React.useState<SettingItem[]>([
 		{
-			setting_name: "setting",
-			display_name: "display",
+			setting_name: "stopplate_indicator_light_duration",
+			display_name: "Stopplate indicator light up duration",
 			type: "slider",
 			max: 10,
 			min: 0,
 			value: 0,
+			unit: "s",
+		},
+		{
+			setting_name: "timer_count_down_range",
+			display_name: "Timer countdown time random range",
+			type: "range",
+			max: 10,
+			min: 0,
+			value: [0, 0],
+			unit: "s",
+		},
+		{
+			setting_name: "buzzer_duration",
+			display_name: "Buzer (beep sound) duration",
+			type: "slider",
+			max: 10,
+			min: 0,
+			value: 0,
+			unit: "s",
+		},
+		{
+			setting_name: "buzzer_frequency",
+			display_name: "Buzzer frequency",
+			type: "slider",
+			max: 10,
+			min: 0,
+			value: 0,
+			unit: "Hz",
+		},
+		{
+			setting_name: "buzzer_waveform",
+			display_name: "Buzzer waveform",
+			type: "selector",
+			selector_item: BUZZER_WAVEFORM_OBJECT,
+			value: 0,
 		},
 	]);
 
-	function onSettingChange(settingName: string, value: number) {
+	function onSettingChange(settingName: string, value: number | number[]) {
 		const newSetting = setting;
 		const index = newSetting.findIndex(v => v.setting_name === settingName);
 		newSetting[index].value = value;
@@ -115,6 +237,7 @@ export default function StopplateSettngDialog(props: DialogProps) {
 				<DialogTitle>Stopplate Settings</DialogTitle>
 				<DialogContent>
 					<Stack>
+						<Button fullWidth variant="outlined">Connect</Button>
 						{setting.map((v, k) => {
 							return <SettingItem
 								key={k}
@@ -125,13 +248,14 @@ export default function StopplateSettngDialog(props: DialogProps) {
 								value={v.value}
 								min={v.min}
 								max={v.max}
+								selector_item={v.selector_item}
 							/>;
 						})}
 					</Stack>
 				</DialogContent>
 				<DialogActions>
-					<Button onClick={props.onClose}>Cancel</Button>
-					<Button type="submit">Subscribe</Button>
+					<Button onClick={props.onClose} variant="contained" color="error" fullWidth>Cancel</Button>
+					<Button type="submit" onClick={props.onClose} variant="contained" color="success" fullWidth>Apply</Button>
 				</DialogActions>
 			</Dialog>
 		</>
