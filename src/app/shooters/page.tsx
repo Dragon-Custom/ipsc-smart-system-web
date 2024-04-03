82dd784c94e5d1c5e4837ee7a610de29619fdded
jkh675 <rluo2018@gmail.com>
2024-04-03 23:31:51 +0800

    imp the shooter card and delete feature

--- src/app/shooters/page.tsx
+++ src/app/shooters/page.tsx
@@ -2,22 +2,51 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Query } from "@/gql/graphql";
-import { AgGridReact } from "ag-grid-react";
+// import { AgGridReact } from "ag-grid-react";
+import ShooterCard from "./shooterCard";
+import { List, SpeedDial, SpeedDialAction, SpeedDialIcon, Typography } from "@mui/material";
+import { Add } from "@mui/icons-material";
 
-const GET_ALL_SHOOTERS_QUERY = gql`
-    query FindManyShooter{
-        findManyShooter {
-            division
-        }
-    }
+const FindManyShooterQuery = gql`
+	query FindManyShooter{
+		findManyShooter {
+			division
+			id
+			createAt
+			name
+		}
+	}
 `;
 
+
 export default function Shooters() {
-	const { data } = useQuery<Query>(GET_ALL_SHOOTERS_QUERY);
+	const { data } = useQuery<Query>(FindManyShooterQuery);
 	return (
 		<>
-			{JSON.stringify(data?.findManyShooter)}
-            Shooters
+			<Typography variant="h4" p={2}>Shooter list: </Typography>
+			<List>
+				{data?.findManyShooter.map((v, k) => (
+					<ShooterCard
+						key={k}
+						createDate={new Date(v.createAt).toLocaleDateString()}
+						division={v.division}
+						name={v.name}
+						id={v.id}
+						showMutationButton
+					/>
+				))}
+			</List>
+			<SpeedDial
+				ariaLabel="Shooters operation"
+				sx={{ position: "absolute", bottom: 16, right: 16 }}
+				icon={<SpeedDialIcon />}
+			>
+				<SpeedDialAction
+					icon={<Add/>}
+					tooltipTitle={"Create shooter"}
+					tooltipOpen
+				/>
+			</SpeedDial>
 		</>
 	);
 }
