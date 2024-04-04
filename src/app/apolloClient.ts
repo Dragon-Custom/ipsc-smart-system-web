321c66da994b8061ff09506a43d2593d76fd62b7
jkh675 <rluo2018@gmail.com>
2024-04-04 13:54:43 +0800

    impl sse link for real time update

--- src/app/apolloClient.ts
+++ src/app/apolloClient.ts
@@ -3,36 +3,62 @@ import {
 	ApolloClient,
 	InMemoryCache,
 	HttpLink,
+	split,
+	ApolloLink,
+	Operation,
+	FetchResult,
 } from "@apollo/client";
 import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { Observable, getMainDefinition } from "@apollo/client/utilities";
+import { Client, ClientOptions, createClient } from "graphql-sse";
 
 /* #region create Websocket link for subscription */
 const httpLink = new HttpLink({
 	uri: process.env.NEXT_PUBLIC_API_ENDPOINT,
+		
 });
 
-// const sseLink = new ServerSentEventsLink({
-// 	graphQlSubscriptionUrl: process.env.backendUrl,
-// });
-// const wsLink = new GraphQLWsLink(
-// 	createClient({
-// 		url: process.env.NEXT_PUBLIC_wsBackendUrl as string,
-// 	})
-// );
-// const splitLink = split(
-// 	({ query }) => {
-// 		const definition = getMainDefinition(query);
-// 		return (
-// 			definition.kind === "OperationDefinition" &&
-// 			definition.operation === "subscription"
-// 		);
-// 	},
-// 	wsLink,
-// 	httpLink
-// );
+class SSELink extends ApolloLink {
+	private client: Client;
+ 
+	constructor(options: ClientOptions) {
+		super();
+		this.client = createClient(options);
+	}
+ 
+	public request(operation: Operation): Observable<FetchResult> {
+		return new Observable((sink) => {
+			return this.client.subscribe<FetchResult>(
+				{ ...operation, query: operation.query.loc?.source.body ?? ""},
+				{
+					next: sink.next.bind(sink),
+					complete: sink.complete.bind(sink),
+					error: sink.error.bind(sink),
+				},
+			);
+		});
+	}
+}
+const sseLink = new SSELink({
+	url: process.env.NEXT_PUBLIC_API_ENDPOINT ?? "",
+});
+
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query);
+		return (
+			definition.kind == "OperationDefinition" &&
+			definition.operation == "subscription"
+		);
+	},
+	sseLink,
+	httpLink,
+);
+
 export const client = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: httpLink,
+	connectToDevTools: true,
+	link: splitLink,
 });
 
 if (process.env.NODE_ENV !== "production") {
