fdc09212dc5d408218e4a008b758321c12d691b8
jkh675 <rluo2018@gmail.com>
2024-04-06 17:13:05 +0800

    impl the create stage feature

--- src/app/stages/stageFormDialog.tsx
+++ src/app/stages/stageFormDialog.tsx
@@ -1,6 +1,6 @@
 import ImageCropper from "@/components/ImageCropper";
-import { Query } from "@/gql/graphql";
-import { gql, useQuery } from "@apollo/client";
+import { Mutation, MutationCreateOneStageArgs, Query } from "@/gql/graphql";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { FileUpload } from "@mui/icons-material";
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Grid, InputAdornment, InputLabel, MenuItem,  Select, Stack, TextField,  styled } from "@mui/material";
 import React from "react";
@@ -37,12 +37,54 @@ const FindManyShooterQuery = gql`
 	}
 `;
 
+const CreateOneStageMutation = gql`
+	mutation($data: StageCreateInput!){
+		createOneStage(data: $data) {
+			createAt
+		}
+	}
+`;
+
+
+export interface StageFormData {
+	name: string;
+	description: string;
+	designer: string;
+	papers: string;
+	noshoots: string;
+	poppers: string;
+	gunCondition: string;
+	walkthroughTime: string;
+}
+
+function extactFromData(event: React.FormEvent<HTMLFormElement>) {
+	const formData = new FormData(event.currentTarget);
+	return Object.fromEntries((formData).entries()) as unknown as StageFormData;
+}
+
+type ImageID = string;
+async function uploadImage(base64url: string): Promise<ImageID> {
+	const formdata = new FormData();
+	formdata.append("0", new File([await (await fetch(base64url)).blob()], "12.jpeg"));
+	formdata.append("map", "{\"0\": [\"variables.image\"]}");
+	formdata.append("operations", "{\"query\": \"mutation ($image: File!) {  uploadImage(image: $image) }\"}");
+
+	const requestOptions = {
+		method: "POST",
+		body: formdata,
+		redirect: "follow",
+	};
+
+	const response = await (await fetch("https://dragoncustom.onflashdrive.app:2087/graphql", requestOptions as RequestInit)).json();
+	return response.data.uploadImage;
+}
 export interface StageFormDialogProps {
 	onClose: () => void;
 	open: boolean;
 }
 export default function StageFormDialog(props: StageFormDialogProps) {
 	const allShooter = useQuery<Query>(FindManyShooterQuery);
+	const [createStage] = useMutation<Mutation["createOneStage"], MutationCreateOneStageArgs>(CreateOneStageMutation);
 	const [stageAttr, setStageAttr] = React.useState({
 		minRounds: 0,
 		maxScore: 0,
@@ -51,14 +93,66 @@ export default function StageFormDialog(props: StageFormDialogProps) {
 	const [cropperOpen, setCropperOpne] = React.useState(false);
 	const [stageImage, setStageImage] = React.useState("");
 
-	function onCreateStageFormSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget);
-		const formJson = Object.fromEntries((formData).entries());
-		console.log(formJson);
-	}
-	function onCreateStageFormChange() {
+	React.useEffect(() => {
+		allShooter.refetch();
+		setStageImage("");
+	}, [props]);
 
+	async function onCreateStageFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const formData = extactFromData(event);
+		console.log(formData);
+		const papers = parseInt(formData.papers);
+		const poppers = parseInt(formData.poppers);
+		const noshoots = parseInt(formData.noshoots);
+		const gunCondition = parseInt(formData.gunCondition);
+		const designer = parseInt(formData.designer);
+		const walkthroughTime = parseInt(formData.walkthroughTime);
+		const imgId = await uploadImage(stageImage);
+		createStage({
+			variables: {
+				data: {
+					image: {
+						connect: {
+							id: imgId,
+						},
+					},
+					name: formData.name,
+					description: formData.description,
+					papers,
+					poppers,
+					noshoots,
+					gunCondition,
+					designer: {
+						connect: {
+							id: designer,
+						},
+					},
+					walkthroughTime,
+				},
+			},
+		});
+	}
+	function onCreateStageFormChange(event: React.FormEvent<HTMLFormElement>) {
+		const formData = extactFromData(event);
+		console.log(formData);
+		const papers = parseInt(formData.papers);
+		const poppers = parseInt(formData.poppers);
+		const minRounds = papers * 2 + poppers;
+		let stage_type: string;
+		if (minRounds <= 12) 
+			stage_type = "Short";
+		else if (minRounds <= 24)
+			stage_type = "Medium";
+		else if (minRounds <= 32)
+			stage_type = "Long";
+		else
+			stage_type = "Unsanctioned";
+		setStageAttr({
+			minRounds,
+			maxScore: papers * 2 * 5 + poppers * 5,
+			stageType: stage_type,
+		});
 	}
 
 	const onPicturePassIn = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -106,7 +200,7 @@ export default function StageFormDialog(props: StageFormDialogProps) {
 							startIcon={<FileUpload />}
 							component="label"
 						>
-							<VisuallyHiddenInput type="file" accept="image/*" onChange={onPicturePassIn} name="image" value={stageImage}/>
+							<VisuallyHiddenInput type="file" accept="image/*" onChange={onPicturePassIn} name="image"/>
 							Upload the stage picture
 						</Button>
 						<img src={stageImage} />
@@ -147,10 +241,13 @@ export default function StageFormDialog(props: StageFormDialogProps) {
 								<TextField
 									fullWidth
 									required
-									name="pappers"
-									label="Pappers"
+									name="papers"
+									label="Papers"
 									type="number"
 									variant="outlined"
+									inputProps={{
+										min: 0,
+									}}
 								/>
 							</Grid>
 							<Grid item xs={12/2} sm={12/3}>
@@ -161,6 +258,9 @@ export default function StageFormDialog(props: StageFormDialogProps) {
 									label="No-shoots"
 									type="number"
 									variant="outlined"
+									inputProps={{
+										min: 0,
+									}}
 								/>
 							</Grid>
 							<Grid item xs={12} sm={12/3}>
@@ -171,6 +271,9 @@ export default function StageFormDialog(props: StageFormDialogProps) {
 									label="Popper"
 									type="number"
 									variant="outlined"
+									inputProps={{
+										min: 0,
+									}}
 								/>
 							</Grid>
 						</Grid>
