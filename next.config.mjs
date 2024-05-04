72edb68e3c0e9415046707a77d2c638abe74d9c6
jkh675 <rluo2018@gmail.com>
2024-05-04 12:42:59 +0800

    impl redirect api

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,14 @@ const NEXT_CONFIG = {
 	experimental: {
 		runtime: "edge", // 'node.js' (default) | experimental-edge
 	},
-	
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: process.env.NEXT_PUBLIC_API_ENDPOINT + "/:path*",
+			},
+		];
+	},
 };
 
 export default NEXT_CONFIG;
