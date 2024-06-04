1d775259c65ee7023072d30cea92136bb3e74b46
jkh675 <rluo2018@gmail.com>
2024-06-04 19:17:48 +0800

    imporve the design for better directive

--- src/app/shooters/shooterCard.tsx
+++ src/app/shooters/shooterCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Delete, Edit } from "@mui/icons-material";
-import { Button, Grid, IconButton, ListItem, ListItemAvatar, ListItemButton, ListItemText, Paper, Stack, Typography, useTheme } from "@mui/material";
+import { Button, ButtonGroup, Grid, Paper, Typography, useTheme } from "@mui/material";
 import { gql, useMutation, useQuery } from "@apollo/client";
 import { Division, Mutation, MutationDeleteShooterArgs, Query, QueryShooterArgs } from "@/gql/graphql";
 import { useConfirm } from "material-ui-confirm";
@@ -100,42 +100,44 @@ export default function ShooterCard(props: ShooterCardProps) {
 
 	return (
 		<>
-			<Paper elevation={2} sx={{my:2, p:0}}>
+			<Paper elevation={5} sx={{my:2, p:0}}>
 				<Grid container gap={2} sx={{p:0}}>
 					<Grid container item xs={12} sm sx={{p:0}}>
-						<Button fullWidth sx={{ p: 1, color: "white", textAlign: "left" }} variant="outlined" color="warning" onClick={() => props?.onClick?.(props.id)}>
+						<Button fullWidth sx={{ p: 1, color: "white", textAlign: "left" }} variant="outlined" color="primary" onClick={() => props?.onClick?.(props.id)}>
 							<Grid container>
 								<Grid item xs={12} sm={12} md={12/4} alignContent={"center"}>
 									<Typography variant="h4">{props.name}</Typography>
 									<Typography variant="caption" color={"GrayText"}>Division: {props.division}</Typography>
 								</Grid>
 								<Grid item xs={12 / 3} md={12/4} alignContent={"center"}>
-									<Paper elevation={3} sx={{p:1, color: theme.palette.info.main}}>
+									<Paper elevation={5} sx={{p:1, color: theme.palette.info.main}}>
 										<Typography variant="button">Rank: #{query.data?.shooter?.rankings?.[0]?.rank}</Typography>
 									</Paper>
 								</Grid>
 								<Grid item xs={12 / 3} md={12/4} alignContent={"center"}>
-									<Paper elevation={3} sx={{p:1, color: theme.palette.secondary.main}}>
+									<Paper elevation={10} sx={{p:1, color: theme.palette.secondary.main}}>
 										<Typography variant="button">Rating: {query.data?.shooter?.ratings?.[0]?.rating.toFixed(1)}</Typography>
 									</Paper>
 								</Grid>
 								<Grid item xs={12 / 3} md={12/4} alignContent={"center"}>
-									<Paper elevation={3} sx={{p:1, color: theme.palette.success.main}}>
+									<Paper elevation={15} sx={{p:1, color: theme.palette.success.main}}>
 										<Typography variant="button">ELO: {query.data?.shooter?.elo?.[0]?.elo.toFixed(1)}</Typography>
 									</Paper>
 								</Grid>
 							</Grid>
 						</Button>
 					</Grid>
-					<Grid item xs={12} sm={2} md={8/4} alignContent={"center"} sx={{p:0}}>
-						<Stack direction="row" justifyContent={"space-around"}>
-							<Button fullWidth onClick={onEditButtonClick} color="inherit">
-								<Edit/>
-							</Button>
-							<Button fullWidth onClick={onDeleteButtonClick} color="error">
-								<Delete/>
-							</Button>
-						</Stack>
+					<Grid item xs={12} sm={2} md={8/4} alignContent={"center"} sx={{p:0}} >
+						<Paper elevation={20} sx={{height: "100%"}}>
+							<ButtonGroup variant="outlined" fullWidth sx={{height: "100%"}}>
+								<Button fullWidth onClick={onEditButtonClick} color="secondary" variant="outlined">
+									<Edit/>
+								</Button>
+								<Button fullWidth onClick={onDeleteButtonClick} color="error" variant="outlined">
+									<Delete/>
+								</Button>
+							</ButtonGroup>
+						</Paper>
 					</Grid>
 				</Grid>
 			</Paper>
